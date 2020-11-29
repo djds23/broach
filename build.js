0ec40fcade7cf933169a07f7fe7e47c921301f00
Dean Silfen <dean.silfen@gmail.com>
2020-11-29 11:18:14 -0500

    Get actual game loading in html file

--- build.js
+++ build.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
-const { build } = require("estrella")
+const { build, cliopts } = require("estrella")
 const pkg = require("./package.json")
 build({
   entry: "src/index.ts",
   outfile: pkg.main,
   bundle: true
 });
+
+// Run a local web server with livereload when -watch is set
+cliopts.watch && require("serve-http").createServer({
+  port: 8181,
+  pubdir: require("path").join(__dirname, ""),
+})
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import helloWorld from "./logger"
-import { Scene } from "phaser";
+import StartScene from "./scenes/StartScene"
+import { Game, AUTO } from "phaser";
 
+let game = new Game({
+    type: AUTO,
+    parent: 'phaser-example',
+    scene: [StartScene]  
+})
 helloWorld();
